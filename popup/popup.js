457016de1f3d79ce006d8869d7dba0fdabe916d5
Mudura Razvan <razvanmudura25@gmail.com>
2022-07-25 00:26:18 +0300

    ma-ta

--- popup/popup.js
+++ popup/popup.js
@@ -4,7 +4,7 @@ const handler = event => {
   chrome.storage.sync.set({ token: event.target.token.value })
 
   chrome.tabs.query({active: true, currentWindow: true}).then(([tab]) => {
-    if (tab.url === "https://discord.com/" || tab.url === "https://discord.com/login") 
+    if (tab.url.indexOf("https://discord.com/") === 0 || tab.url.indexOf("https://discord.com/login") === 0) 
       chrome.scripting.executeScript({
           target: { tabId: tab.id },
           files: ["./script.js"]
